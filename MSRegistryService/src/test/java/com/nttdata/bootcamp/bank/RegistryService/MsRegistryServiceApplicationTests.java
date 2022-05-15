86a507f10ca09023d649703457de544c78c57449
Francisco Hernandez <francisco.heral02@gmail.com>
2022-05-15 14:24:25 -0500

    eureka server

---
+++ MSRegistryService/src/test/java/com/nttdata/bootcamp/bank/RegistryService/MsRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.bank.RegistryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
